217d9e6f41d05e786e48deb08dbdcd5195303e8b
ruhy567 <galiba.chowdhury56@gmail.com>
2024-11-13 00:17:12 +0600

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const number = [56, 82, 93, 34, 05];
-//console.log(number.length);
+console.log(number.length);
 //get element value by index
-//console.log(number[3]);
+console.log(number[3]);
 const third = number[2];
 console.log(third);
 
