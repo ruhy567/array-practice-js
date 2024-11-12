af9c06337787e5259f96e69dc82106ae3f783a37
ruhy567 <galiba.chowdhury56@gmail.com>
2024-11-13 00:15:40 +0600

    all

---
+++ index.js
@@ -0,0 +1,12 @@
+const number = [56, 82, 93, 34, 05];
+//console.log(number.length);
+//get element value by index
+//console.log(number[3]);
+const third = number[2];
+console.log(third);
+
+//set or update element in array
+let ab = 45;
+ab = 98;
+number[4] = 55;
+console.log(number);
